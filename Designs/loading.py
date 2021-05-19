9451f1d8cb9b6fd95d10f138c39376b963b0fea2
MRprgmr <algoprogram01@gmail.com>
2021-05-19 11:33:51 +0500

    New changes:

--- Designs/loading.py
+++ Designs/loading.py
@@ -56,7 +56,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Loading datebase.."))
         self.label_2.setText(_translate("MainWindow", "Ma\'lumotlar yuklanyapti..."))
 import Res_rc
 
